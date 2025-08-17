b2a3a92e1ffcb102ef7bfd12c92369c081c8e332
Awej Pathan <awejofficial@gmail.com>
2025-08-17 11:34:47 +0000

    Day4 lines

---
+++ src/main/java/com/ApnaCollegeProblems/Day4/lines.java
@@ -0,0 +1,19 @@
+package com.ApnaCollegeProblems.Day4;
+import java.util.*;
+public class lines {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+             int n = sc.nextInt();
+        //     int i=1;
+        //     while(i<=n){
+        //         System.out.println("****");
+        //         i++;
+        //     }
+        for(int i=1;i<=n;i++){
+            System.out.println("XXXX");
+        }
+        
+        
+    }
+    
+}
