3de6069425fed7061d5cc1b2b559dfb8dc25cc8a
Awej Pathan <awejofficial@gmail.com>
2025-08-17 12:44:38 +0000

    reverse number

---
+++ src/main/java/com/ApnaCollegeProblems/Day4/reverse.java
@@ -0,0 +1,18 @@
+package com.ApnaCollegeProblems.Day4;
+import java.util.*;
+
+public class reverse {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while(n>0){
+            int lastdigit = n%10;
+            System.out.print(lastdigit);
+            n=n/10; //deltes the last digit
+
+        }
+        System.out.println();
+
+    }
+    
+}
