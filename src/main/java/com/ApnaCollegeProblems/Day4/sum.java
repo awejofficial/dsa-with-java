521f2951e5ecac2c31e08e1a6b8fba449e944c2e
Awej Pathan <awejofficial@gmail.com>
2025-08-17 11:15:42 +0000

    Day4

---
+++ src/main/java/com/ApnaCollegeProblems/Day4/sum.java
@@ -0,0 +1,19 @@
+package com.ApnaCollegeProblems.Day4;
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum=0;
+        int i =1;
+        while(i<= n){
+            sum+=i;
+            i++;
+        }
+
+        System.out.println("Sum Of First Natural n Numbers: "+sum);
+
+    }    
+    
+}
---
+++ src/main/java/com/LeetcodeProblems/TwoSums.java
@@ -0,0 +1,29 @@
+# Intuition
+<!-- Describe your first thoughts on how to solve this problem. -->
+
+# Approach
+<!-- Describe your approach to solving the problem. -->
+
+# Complexity
+- Time complexity:
+<!-- Add your time complexity here, e.g. $$O(n)$$ -->
+
+- Space complexity:
+<!-- Add your space complexity here, e.g. $$O(n)$$ -->
+
+# Code
+```java []
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{}; // No solution (problem guarantees one exists, so this won't run)
+    }
+}
+
+```
