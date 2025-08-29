6daf9fec3d1855f02e46267c9d40a643dc65ab21
Awej Pathan <awejofficial@gmail.com>
2025-08-29 09:47:13 +0000

    Smallest and The Largest Number In an Array

--- src/main/java/com/ApnaCollegeProblems/Array/Smallest_Largest_Numbers.java
+++ src/main/java/com/ApnaCollegeProblems/Array/Smallest_Largest_Numbers.java
@@ -1,7 +1,7 @@
 package com.ApnaCollegeProblems.Array;
 import java.util.*;
 
-public class largestNumber {
+public class Smallest_Largest_Numbers {
     public static int getLargest(int numbers[]) {
         int largest = Integer.MIN_VALUE;
         for(int i=0; i<numbers.length; i++) {
@@ -11,10 +11,21 @@ public class largestNumber {
         }
         return largest;
     }
+    public static int getSmallest(int numbers[]) {
+        int smallest = Integer.MAX_VALUE;
+        for(int i=0; i<numbers.length; i++) {
+            if (smallest > numbers[i]) {
+                smallest = numbers[i];
+            }
+        }
+        return smallest;
+    }
     public static void main(String args[]){
         int numbers[] = {1,2,4,2,6,8,2,9};
         int largest = getLargest(numbers);
+        int smallest = getSmallest(numbers);
         System.out.println("largest Number in the Numbers:"+ largest);
+        System.out.println("Smallest Number in the Numbers:"+ smallest);
     }
     
 }
