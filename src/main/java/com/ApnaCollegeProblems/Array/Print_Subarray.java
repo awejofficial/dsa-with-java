5e2b4b7a1050f73fdc77c81c22fce4efc7844cfc
Awej Pathan <awejofficial@gmail.com>
2025-09-01 06:07:30 +0000

    Subarrays

---
+++ src/main/java/com/ApnaCollegeProblems/Array/Print_Subarray.java
@@ -0,0 +1,23 @@
+package com.ApnaCollegeProblems.Array;
+
+public class Print_Subarray {
+    public static void printSubarrays(int numbers[]) {
+        int totSubArray = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i; j < numbers.length; j++) {
+                for (int k = i; k <= j; k++) {
+                    System.out.print(numbers[k] + " ");
+                }
+                totSubArray++;
+                System.out.println();
+            }
+            System.out.println();
+        }
+        System.out.println("Total number of subarrays: " + totSubArray);
+    }
+
+    public static void main(String args[]) {
+        int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        printSubarrays(numbers);
+    }
+}
