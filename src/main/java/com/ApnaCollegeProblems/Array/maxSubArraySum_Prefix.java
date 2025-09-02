ea5ef0f0d2e76a239f71f15d834452ede4b41316
Awej Pathan <awejofficial@gmail.com>
2025-09-02 07:24:02 +0000

    errors

--- src/main/java/com/ApnaCollegeProblems/Array/maxSubArraySum_Prefix.java
+++ src/main/java/com/ApnaCollegeProblems/Array/maxSubArraySum_Prefix.java
@@ -5,6 +5,21 @@ public class MaxSubarraySum_Prefix {
 
     // Method to find the maximum subarray sum using the prefix sum approach
     public static void maxSubarraySumPrefix(int numbers[]) {
+        int maxSum = Integer.MIN_VALUE;
+        int curr =0;
+
+        int prefix[] = new int [numbers.length];
+        prefix[0] = numbers[0];
+        for (int i =1;i<numbers.length; i++) {
+            prefix[i] = prefix[i-1] + numbers[i];
+        }
+        for (int i =0; i<numbers.length; i++0) {
+            for (int j=i; j<numbers.length; j++) {
+                if (i == 0) {
+                    curr = prefix[];
+                }
+            }
+        }
         
     }
 
