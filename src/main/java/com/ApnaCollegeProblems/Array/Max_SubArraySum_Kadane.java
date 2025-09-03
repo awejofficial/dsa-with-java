2f54c6749831bd22077af637b79da70b33f2c030
Awej Pathan <awejofficial@gmail.com>
2025-09-03 15:07:38 +0000

    kadanes

--- src/main/java/com/ApnaCollegeProblems/Array/Max_SubArraySum_Kadane.java
+++ src/main/java/com/ApnaCollegeProblems/Array/Max_SubArraySum_Kadane.java
@@ -1,3 +1,21 @@
+package com.ApnaCollegeProblems.Array;
+
 public class Max_SubArraySum_Kadane {
-    
+    public static void kadanes(int numbers[]) {
+        int cs =0;
+        int ms =Integer.MIN_VALUE;
+        for(int i=0; i<numbers.length; i++) {
+            cs= cs+ numbers[i];
+            if (cs<0) {
+                cs = 0;
+            }
+            ms=Math.max(cs, ms);            
+        }
+        // The print statement should be outside the loop to print the final maximum sum.
+        System.out.println("Our Max Subarray sum is: "+ms);
+    }
+    public static void main(String[] args) {
+        int numbers[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        kadanes(numbers);
+    }
 }
