9df27639123f9e4258fe05a7dc4d9feb7fd7a86c
Awej Pathan <awejofficial@gmail.com>
2025-09-07 07:38:14 +0000

    .

---
+++ src/main/java/com/ApnaCollegeProblems/Array/Trapped_Rainwater.java
@@ -0,0 +1,8 @@
+package com.ApnaCollegeProblems.Array;
+public class Trapped_Rainwater {
+    public static int TrapedWater (int height[]) {
+
+    }
+    public static void main(String args [])
+    
+}
