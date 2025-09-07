adbf16cbc5e76b929707a0eb68c81b0e05ce1862
Awej Pathan <awejofficial@gmail.com>
2025-09-07 08:17:27 +0000

    Rainwater

--- src/main/java/com/ApnaCollegeProblems/Array/Trapped_Rainwater.java
+++ src/main/java/com/ApnaCollegeProblems/Array/Trapped_Rainwater.java
@@ -1,35 +1,32 @@
 package com.ApnaCollegeProblems.Array;
+
 public class Trapped_Rainwater {
-    public static int TrapedWater (int height[]) {
-        //calculate the left max boundry- array
+    public static int TrapedWater(int height[]) {
         int h = height.length;
-        int leftMax = new int[h];
+
+        int leftMax[] = new int[h];
         leftMax[0] = height[0];
-        for(int i=1; i<h;i++) {
-            leftMax[i] = Math.max(height[i],leftMax[i]);
+        for (int i = 1; i < h; i++) {
+            leftMax[i] = Math.max(height[i], leftMax[i - 1]);
         }
 
-        //calculate the right max boundry - array
-        int rightMax = new int[h];
-        rightMax[h-1] = height[h-1];
-        for(int i=n-2; i<=0; i--) {
-            rightMax[i] = Math.max(height[i],rightMax[i+1])
-        } 
-        //loop
-        int trappedWater =0;
-        for(int i=0 ; i<h; i++) {
-            int waterlevel = Math.min(leftMax[i],rightMax[i]);
+        int rightMax[] = new int[h];
+        rightMax[h - 1] = height[h - 1];
+        for (int i = h - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(height[i], rightMax[i + 1]);
+        }
+
+        int trappedWater = 0;
+        for (int i = 0; i < h; i++) {
+            int waterlevel = Math.min(leftMax[i], rightMax[i]);
             trappedWater += waterlevel - height[i];
         }
+
         return trappedWater;
-        //waterlevel= min(leftmaxBoundry,rightmaxBundry)
-        //trapped water = waterlevel - height[i]
-
-
     }
-    public static void main(String args []) {
-        int height[] ={4,2,0,6,3,2,5};
 
+    public static void main(String args[]) {
+        int height[] = {4, 2, 0, 6, 3, 2, 5};
+        System.out.println("Total Trapped Water: " + TrapedWater(height));
     }
-    
 }
