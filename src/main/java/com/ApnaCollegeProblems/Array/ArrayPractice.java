d1a8fd05ece5efcdda4f513f29f7884b5277a20f
Awej Pathan <awejofficial@gmail.com>
2025-09-19 14:27:51 +0000

    king

---
+++ src/main/java/com/ApnaCollegeProblems/Array/ArrayPractice.java
@@ -0,0 +1,35 @@
+package com.ApnaCollegeProblems.Array;
+
+public class ArrayPractice {
+    public static int Trapped_Rainwater (int height[]) {
+        int h = height.length;
+
+        // left
+        int leftMax[] = new int[h];
+        leftMax[0] = height[0];
+        for (int i =1; i<h; i++) {
+            leftMax[i] = Math.max(height[i], leftMax[i-1]);
+        }
+        
+
+         // right
+         int rightMax[] = new int[h];
+         rightMax[h-1] = height[h-1];
+         for (int i =h-2; i>=0; i--) {
+             rightMax[i] = Math.max(height[i], rightMax[i+1]);
+         }
+
+         //Loop
+         int trappedWater =0;
+         for (int i=0;i<h;i++){
+            int waterLevel = Math.min(leftMax[i], rightMax[i]);
+            trappedWater += waterLevel - height[i];
+         }
+         return trappedWater;
+    }
+
+    public static void main(String[] args) {
+        int height[] = {4, 2, 0, 6, 3, 2, 5};
+        System.out.println("Total Trapped Water: " + Trapped_Rainwater(height));
+    }
+}
